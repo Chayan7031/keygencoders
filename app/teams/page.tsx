262bc6223fdffcc476a448764dacc4f4b387672f
sayanChaterjee <chatterjeesayan1009@gmail.com>
2025-03-01 19:19:10 +0530

    teams commit

---
+++ app/teams/page.tsx
@@ -0,0 +1,55 @@
+'use client'
+
+import Image from 'next/image'
+import React from 'react'
+import { thirdYearMembers, secondYearMembers, firstYearInterns, Member, fourthYearMembers } from '@/app/utils/data'
+
+const MemberGrid = ({ members, title }: { members: Member[]; title: string }) => {
+  return (
+    <>
+      <div className="p-4 mx-auto relative z-10 w-full pt-10 md:pt-20 px-2">
+        <div className="md:text-4xl text-2xl text-center bg-clip-text text-transparent bg-gradient-to-b from-green-500 to-neutral-500 bg-opacity-50">
+          {title}
+        </div>
+      </div>
+
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4 py-10">
+        {members.map((member, index) => (
+          <div
+            key={index}
+            className="group aspect-square overflow-hidden rounded-lg relative"
+          >
+
+            <Image
+              width={500}
+              height={500}
+              priority
+              className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
+              src={member.imageUrl}
+              alt={member.name}
+            />
+
+            <div className="absolute inset-0 bg-black bg-opacity-0 transition-opacity duration-300 group-hover:bg-opacity-50"></div>
+
+            <div className="absolute inset-x-0 bottom-0 p-4 text-white text-center translate-y-full transition-transform duration-500 group-hover:-translate-y-1/2">
+              <p className="md:text-lg text-sm font-semibold">{member.name}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  )
+}
+
+const Page = () => {
+  return (
+    <div className="text-white p-2">
+      <MemberGrid members={fourthYearMembers} title="Our Convenors" />
+      <MemberGrid members={thirdYearMembers} title="Third year members" />
+      <MemberGrid members={secondYearMembers} title="Second year members" />
+      <MemberGrid members={firstYearInterns} title="First year interns" />
+    </div>
+  )
+}
+
+export default Page
---
+++ app/utils/data.ts
@@ -0,0 +1,214 @@
+export interface Member {
+  name: string;
+  imageUrl: string;
+}
+
+
+export const fourthYearMembers: Member[] = [
+  {
+    name: "Jyotirmay Bairagya",
+    imageUrl:
+      "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740824729/user_q6pp8i.avif",
+  },
+  {
+    name: "Ushasi Das",
+    imageUrl:
+      "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740824729/user_q6pp8i.avif",
+  },
+  {
+    name: "Arko Kundu",
+    imageUrl:
+      "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740824729/user_q6pp8i.avif",
+  },
+  {
+    name: "Debamrita Paul",
+    imageUrl:
+      "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740824729/user_q6pp8i.avif",
+  },
+
+]
+
+export const thirdYearMembers: Member[] = [
+  {
+    name: "Jyotirmay Bairagya",
+    imageUrl:
+      "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740824729/user_q6pp8i.avif",
+  },
+  {
+    name: "Ushasi Das",
+    imageUrl:
+      "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740824729/user_q6pp8i.avif",
+  },
+  {
+    name: "Arko Kundu",
+    imageUrl:
+      "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740824729/user_q6pp8i.avif",
+  },
+  {
+    name: "Debamrita Paul",
+    imageUrl:
+      "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740824729/user_q6pp8i.avif",
+  },
+  {
+    name: "Archismita Ghosh",
+    imageUrl:
+      "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740824729/user_q6pp8i.avif",
+  },
+  // {
+  //   name: "Alice Johnson",
+  //   imageUrl: "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740824729/user_q6pp8i.avif",
+  // },
+  // {
+  //   name: "John Doe",
+  //   imageUrl: "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg",
+  // },
+  // {
+  //   name: "Jane Smith",
+  //   imageUrl: "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg",
+  // },
+  // {
+  //   name: "Alice Johnson",
+  //   imageUrl: "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg",
+  // },
+];
+
+export const secondYearMembers: Member[] = [
+  {
+    name: "Madhurima Khan",
+    imageUrl:
+      "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740823692/madhurima_khan_jexrvx.jpg",
+  },
+  {
+    name: "Asif Iqbal",
+    imageUrl:
+      "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740823670/asif_iqbal_txp7db.jpg",
+  },
+  {
+    name: "Rohit Chowdhury",
+    imageUrl:
+      "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740823669/Rohit_chowdhury_a30xhk.jpg",
+  },
+  {
+    name: "Shatakshi Saha",
+    imageUrl:
+      "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740823667/shatakshi_saha_aespdr.jpg",
+  },
+  {
+    name: "Sourashmi Das",
+    imageUrl:
+      "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740829069/sourashmi_das_koticy.jpg",
+  },
+  {
+    name: "Rohit Karmakar",
+    imageUrl:
+      "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740829068/rohit_karmakar_dinojc.jpg",
+  },
+  {
+    name: "Nayan Banik",
+    imageUrl:
+      "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740829067/nayan_banik_rvilxm.jpg",
+  },
+  {
+    name: "Sourajeet Routh",
+    imageUrl:
+      "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740829066/sourajeet_vzes3d.jpg",
+  },
+  {
+    name: "Krishanu Biswas",
+    imageUrl:
+      "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740829174/krishanu_biswas_fazo9k.jpg",
+  },
+];
+
+export const firstYearInterns: Member[] = [
+  {
+    name: "Vansh Deo",
+    imageUrl:
+      "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740823671/vansh_deo_tdlds0.jpg",
+  },
+  {
+    name: "Arpan Biswas",
+    imageUrl:
+      "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740823668/arpan_biswas_dygvp8.jpg",
+  },
+  {
+    name: "Aishika Biswas",
+    imageUrl:
+      "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740823669/aishika_biswas_kxdi1m.jpg",
+  },
+  {
+    name: "Abhishek Modak",
+    imageUrl:
+      "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740824309/abhishek_modak_pwxti8.jpg",
+  },
+  {
+    name: "Rohit Majumder",
+    imageUrl:
+      "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740824301/rohit_majumdar_asmtth.jpg",
+  },
+  {
+    name: "Srijita Sahu",
+    imageUrl:
+      "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740824301/srijita_sahu_ye6cas.jpg",
+  },
+  {
+    name: "Sovan Mondal",
+    imageUrl:
+      "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740825355/sovan_mondal_ozv3wu.jpg",
+  },
+  {
+    name: "Samiksha Bajoria",
+    imageUrl:
+      "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740825356/samiksha_bajoria_guf5ol.jpg",
+  },
+  {
+    name: "Chayan Samanta",
+    imageUrl:
+      "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740825355/chayan_samanta_vud6jh.jpg",
+  },
+  {
+    name: "Soujanya Mallick",
+    imageUrl:
+      "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740825355/soujanya_mallick_twkfqf.jpg",
+  },
+  {
+    name: "Aditya Thakur",
+    imageUrl:
+      "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740825355/aditya_thakur_n15mta.jpg",
+  },
+  {
+    name: "Surya Kanta Pal",
+    imageUrl:
+      "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740825722/surya_kanta_pal_dh7tjr.jpg",
+  },
+  {
+    name: "Ipshita Karar",
+    imageUrl:
+      "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740826629/ipshita_karar_wejahy.jpg",
+  },
+  {
+    name: "Koushiki Baral",
+    imageUrl:
+      "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740826629/koushiki_baral_sj1jpt.jpg",
+  },
+  {
+    name: "Aishee Sen",
+    imageUrl:
+      "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740826631/aishee_sen_g5tuon.jpg",
+  },
+  {
+    name: "Shubhabrota Pal",
+    imageUrl:
+      "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740828908/shubhobrata_pal_wi7711.jpg",
+  },
+  {
+    name: "Tousif Azam",
+    imageUrl:
+      "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740829336/Tousif_azam_nzsdxk.jpg",
+  },
+  {
+    name: "Navneet Kumar Singh",
+    imageUrl:
+      "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740828909/navneet_singh_ypow9t.jpg",
+  },
+];
