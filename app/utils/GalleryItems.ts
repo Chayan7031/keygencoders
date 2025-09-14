61eb1a846fb06450dc45abea585bb3f24a74c866
Chayan <chayansamanta406@gmail.com>
2025-09-15 01:24:05 +0530

    galary change

--- app/utils/GalleryItems.ts
+++ app/utils/GalleryItems.ts
@@ -24,32 +24,32 @@ export interface GalleryItem {
       id: 5,
       imageNew: "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740845744/events7_jjxrjc.png",
     },
-    // {
-    //   id: 6,
-    //   imageNew: "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740845748/events9_nl4aji.png",
-    // },
+    {
+      id: 6,
+      imageNew: "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740845748/events9_nl4aji.png",
+    },
     {
       id: 7,
       imageNew: "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740845745/events8_z9ismy.png",
     },
-    // {
-    //   id: 9,
-    //   imageNew: "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740845746/events11_vw1qld.png",
-    // },
-    // {
-    //   id: 8,
-    //   imageNew: "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740845745/events8_z9ismy.png",
-    // },
-    // {
-    //   id: 10,
-    //   imageNew: "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740845744/events7_jjxrjc.png",
-    // },
-    // {
-    //   id: 11,
-    //   imageNew: "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740845741/events1_mkzmrb.jpg",
-    // },
-    // {
-    //   id: 12,
-    //   imageNew: "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740845746/events11_vw1qld.png",
-    // }
+    {
+      id: 9,
+      imageNew: "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740845746/events11_vw1qld.png",
+    },
+    {
+      id: 8,
+      imageNew: "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740845745/events8_z9ismy.png",
+    },
+    {
+      id: 10,
+      imageNew: "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740845744/events7_jjxrjc.png",
+    },
+    {
+      id: 11,
+      imageNew: "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740845741/events1_mkzmrb.jpg",
+    },
+    {
+      id: 12,
+      imageNew: "https://res.cloudinary.com/dlxpcyiin/image/upload/v1740845746/events11_vw1qld.png",
+    }
   ];
