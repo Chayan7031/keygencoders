486c5d0bf1546a548a1e674ced2b6d2baa3bb52c
Chayan <chayansamanta406@gmail.com>
2025-09-18 21:43:47 +0530

    brands update

--- app/brands.tsx
+++ app/brands.tsx
@@ -19,6 +19,21 @@ const logos = [
   {
     image: "https://res.cloudinary.com/db9l85phg/image/upload/v1757922371/WhatsApp_Image_2025-09-15_at_12.52.21_0460e1cb-removebg-preview_1_syzkz8.png",
   },
+  {
+    image: "https://res.cloudinary.com/db9l85phg/image/upload/v1758211561/4d5bd669826c15d6acfa0c0817511db4_icon-removebg-preview_yry3ps.png",
+  },
+  {
+    image: "https://res.cloudinary.com/db9l85phg/image/upload/v1758210800/HackerRank_Icon-1000px-removebg-preview_as4cwu.png",
+  },
+  {
+    image: "https://res.cloudinary.com/db9l85phg/image/upload/v1758210905/630248973183b_unstop_logo-removebg-preview_rz7txt.png",
+  },
+  {
+    image: "https://res.cloudinary.com/db9l85phg/image/upload/v1758211121/ueScU0LV_400x400-removebg-preview_yyqdce.png",
+  },
+  {
+    image: "https://res.cloudinary.com/db9l85phg/image/upload/v1758211636/images_imavpp.png",
+  },
 ];
 
 const Brands = () => {
@@ -35,7 +50,7 @@ const Brands = () => {
           brands elevate their business.
         </p> */}
 
-        <div className="grid grid-cols-1 md:grid-cols-3 items-center justify-center  mx-auto md:w-3/5 cursor-pointer">
+        <div className="grid grid-cols-2 md:grid-cols-4 items-center justify-center  mx-auto md:w-3/5 cursor-pointer">
           {logos.map((logo, i) => (
             <div key={i} className="p-10 sm:p-5 lg:p-10">
               <Image
@@ -44,7 +59,7 @@ const Brands = () => {
                 width={800}
                 height={800}
                 alt="logo"
-                className="w-full h-auto max-w-full rounded-lg"
+                className="w-full h-auto max-w-full rounded-lg shadow-[0_0_10px_#22c55e] hover:shadow-[0_0_40px_#22c55e] transition duration-300"
               />
             </div>
           ))}
